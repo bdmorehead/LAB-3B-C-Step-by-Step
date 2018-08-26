27dc452580e1ba73941788fef45c530e2c7f15c5
Bobby Morehead <bobby.morehead@outlook.com>
2018-08-26 19:41:25 -0400

    Complete

--- MathsOperators/MainPage.xaml.cs
+++ MathsOperators/MainPage.xaml.cs
@@ -29,28 +29,53 @@ namespace MathsOperators
 
         private void calculateClick(object sender, RoutedEventArgs e)
         {
-            if ((bool)addition.IsChecked)
-            {
-                addValues();
-            }
-            else if ((bool)subtraction.IsChecked)
-            {
-                subtractValues();
-            }
-            else if ((bool)multiplication.IsChecked)
-            {
-                multiplyValues();
-            }
-            else if ((bool)division.IsChecked)
-            {
-                divideValues();
-            }
-            else if ((bool)remainder.IsChecked)
-            {
-                remainderValues();
-            }
-        }
+                
+                try
 
+            { 
+
+                if ((bool)addition.IsChecked)
+                {
+                    addValues();
+                }
+                else if ((bool)subtraction.IsChecked)
+                {
+                    subtractValues();
+                }
+                else if ((bool)multiplication.IsChecked)
+                {
+                    multiplyValues();
+                }
+                else if ((bool)division.IsChecked)
+                {
+                    divideValues();
+                }
+                else if ((bool)remainder.IsChecked)
+                {
+                    remainderValues();
+                }
+                else
+                {
+                    throw new InvalidOperationException("No operator selected");
+                }
+            }   
+                catch (FormatException fEx)
+                {
+                    result.Text = fEx.Message;
+                }
+                catch (OverflowException oEx)
+                {
+                result.Text = oEx.Message;
+                }
+                catch (InvalidOperationException ioEx)
+                {
+                    result.Text = ioEx.Message;
+                }
+                catch (Exception ex)
+                {
+                    result.Text = ex.Message;
+                }
+        }    
         private void addValues()
         {
             int lhs = int.Parse(lhsOperand.Text);
@@ -79,7 +104,7 @@ namespace MathsOperators
             int rhs = int.Parse(rhsOperand.Text);
             int outcome = 0;
 
-            outcome = lhs * rhs;
+            outcome = checked(lhs * rhs);
             expression.Text = $"{lhs} * {rhs}";
             result.Text = outcome.ToString();
         }
